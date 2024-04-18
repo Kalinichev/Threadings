cfb01e69c6c5e159467839178a2f0fa33e202fb8
Kalinichev <kalinichev.peter@yandex.ru>
2024-04-19 01:47:34 +0300

    2 threads

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ class Treading(threading.Thread):
 
 
 def get_data(data):
-    print(f"<<< {threading.current_thread().name}: {data} >>>")
-    time.sleep(5)
+    while True:
+        print(f"<<< {threading.current_thread().name}: {data} >>>")
+        time.sleep(1)
 
 
 thr = threading.Thread(target=get_data, args=(str(time.time()),), name="Thread_1")
 thr.start()
+
+for i in range(100):
+    print(f'Number in range: {i}')
+    time.sleep(1)
