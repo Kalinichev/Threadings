2f65121f46f96ed90e3039cbe1de96e2fb50d879
Kalinichev <kalinichev.peter@yandex.ru>
2024-04-19 02:09:32 +0300

    some threading's methods

--- main.py
+++ main.py
@@ -8,13 +8,23 @@ class Treading(threading.Thread):
 
 def get_data(data):
     while True:
-        print(f"<<< {threading.current_thread().name}: {data} >>>")
+        print(f"<<< {threading.current_thread().name}: {data} >>>\n")
         time.sleep(1)
 
 
 thr = threading.Thread(target=get_data, args=(str(time.time()),), name="Thread_1")
 thr.start()
 
+
+print('old_name:', threading.main_thread().name)
+threading.main_thread().name = 'result'
+print('new_name:', threading.main_thread().name)
+
 for i in range(100):
     print(f'Number in range: {i}')
     time.sleep(1)
+
+    if i % 10 == 0:
+        print('active threads', threading.active_count())
+        print('enumerate:', threading.enumerate())
+        print('Thread_1 is alive:', thr.is_alive())
