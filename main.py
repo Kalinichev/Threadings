fe61c738419c3d358a580a1225a7a91e8a65892a
Kalinichev <kalinichev.peter@yandex.ru>
2024-04-19 02:22:46 +0300

    some threadings

--- main.py
+++ main.py
@@ -2,29 +2,30 @@ import threading
 import time
 
 
-class Treading(threading.Thread):
-    pass
-
-
-def get_data(data):
-    while True:
-        print(f"<<< {threading.current_thread().name}: {data} >>>\n")
+def get_data(data, value):
+    for _ in range(value):
+        print(f"<<< {threading.current_thread().name}: {data} >>>")
         time.sleep(1)
 
 
-thr = threading.Thread(target=get_data, args=(str(time.time()),), name="Thread_1")
-thr.start()
+thr_list = []
 
+for i in range(5):
+    thr = threading.Thread(target=get_data, args=(str(time.time()), i,), name=f'Thread_{i}')
+    thr_list.append(thr)
+    thr.start()
 
-print('old_name:', threading.main_thread().name)
-threading.main_thread().name = 'result'
-print('new_name:', threading.main_thread().name)
-
-for i in range(100):
-    print(f'Number in range: {i}')
-    time.sleep(1)
-
-    if i % 10 == 0:
-        print('active threads', threading.active_count())
-        print('enumerate:', threading.enumerate())
-        print('Thread_1 is alive:', thr.is_alive())
+print(thr_list)
+#
+# print('old_name:', threading.main_thread().name)
+# threading.main_thread().name = 'result'
+# print('new_name:', threading.main_thread().name)
+#
+# for i in range(100):
+#     print(f'Number in range: {i}')
+#     time.sleep(1)
+#
+#     if i % 10 == 0:
+#         print('active threads', threading.active_count())
+#         print('enumerate:', threading.enumerate())
+#         print('Thread_1 is alive:', thr.is_alive())
